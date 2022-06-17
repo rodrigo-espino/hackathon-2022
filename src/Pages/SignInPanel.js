6ca234d2a06d6cca522e3252e095d1c74117b87f
Jorge Colchado Avalos <jorgecolchado.a@gmail.com>
2022-06-17 17:58:25 -0500

    Done

--- src/Pages/SignInPanel.js
+++ src/Pages/SignInPanel.js
@@ -1,13 +1,50 @@
 import React from 'react'
 
-export  function Main()  {
+export  function SignInPanel()  {
   return (
-    <div>Main</div>
+   
+            <div className="contenedor__todo">
+                <div className="caja__trasera">
+                    <div className="caja__trasera-login">
+                        <h3>¿Ya tienes una cuenta?</h3>
+                        <p>Inicia sesión para entrar en la página</p>
+                        <button id="btn__iniciar-sesion">Iniciar Sesión</button>
+                    </div>
+                    <div className="caja__trasera-register">
+                        <h3>¿Aún no tienes una cuenta?</h3>
+                        <p>Regístrate para que puedas iniciar sesión</p>
+                        <button id="btn__registrarse">Regístrarse</button>
+                    </div>
+                </div>
+
+                --Formulario de Login y registro--
+                
+                <div className="contenedor__login-register">
+                   
+                    --Login--
+                    
+                    <form action="" className="formulario__login">
+                        <h2>Iniciar Sesión</h2>
+                        <input type="text" placeholder="Correo Electronico"/>
+                        <input type="password" placeholder="Contraseña"/>
+                        <button>Entrar</button>
+                    </form>
+
+                    --Register--
+                    <form action="" class="formulario__register">
+                        <h2>Regístrarse</h2>
+                        <input type="text" placeholder="Nombre completo"/>
+                        <input type="text" placeholder="Correo Electronico"/>
+                        <input type="text" placeholder="Usuario"/>
+                        <input type="password" placeholder="Contraseña"/>
+                        <button>Regístrarse</button>
+                    </form>
+                </div>
+            </div>
+
+        
+  )
+
   
   
-  
-  
-    )
 }
-
-
