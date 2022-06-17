c796c5358e55b7c8ffe79293fd42e6a65624becb
Uriel Carrasco <duran.jonathan.isw@unipolidgo.edu.mx>
2022-06-17 14:20:50 -0500

    cuidado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 //Pages
 import {Main} from './Pages/Main'
+import {Cuidadanos} from './Pages/Cuidadanos'
  
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
        <Route path="/" element={  <Main/> }/>
+       <Route path="/uriel" element={  <Cuidadanos/>}/>
     </Routes>
+    
   )
 }
 export default App
---
+++ src/Pages/Cuidadanos.js
@@ -0,0 +1,40 @@
+import React from 'react'
+export function Cuidadanos () {
+    return (
+<div className="container">
+  <div className="row align-items-start">
+    <div className="col">
+      One of three columns
+    </div>
+    <div className="col">
+      One of three columns
+    </div>
+    <div className="col">
+      One of three columns
+    </div>
+  </div>
+  <div className="row align-items-center">
+    <div className="col">
+      One of three columns
+    </div>
+    <div className="col">
+      One of three columns
+    </div>
+    <div className="col">
+      One of three columns
+    </div>
+  </div>
+  <div className="row align-items-end">
+    <div className="col">
+      One of three columns
+    </div>
+    <div className="col">
+      One of three columns
+    </div>
+    <div className="col">
+      One of three columns
+    </div>
+  </div>
+</div>
+    )
+}
