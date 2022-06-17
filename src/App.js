079883e2df1dc7cc9ec939365e8c5876e68c2022
Rodrigo Espino <rodrigosantacruz42@gmail.com>
2022-06-17 14:22:52 -0500

    Creating: Candidatos.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
 //Pages
+import { Candidatos } from './Pages/candidatos';
 import {Main} from './Pages/Main'
  
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
+    <div className="col-md-4">
+      <div className="p-4">
     <Routes>
        <Route path="/" element={  <Main/> }/>
+       
+       
+       <Route path="/candidatos" element={  <Candidatos/> }/>
+       
     </Routes>
+    </div>
+    </div>
   )
 }
 export default App
---
+++ src/Pages/candidatos.js
@@ -0,0 +1,7 @@
+import React from 'react'
+export function Candidatos() {
+  return (
+    <h1>Hola</h1>
+  )
+}
+
