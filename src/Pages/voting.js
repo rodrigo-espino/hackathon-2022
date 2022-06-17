c5369d45e1855f4e89b5ba4d98c814a404e792f1
Carlos Mora <eduardo.53.cm@gmail.com>
2022-06-17 14:30:40 -0500

    perre

--- src/Pages/voting.js
+++ src/Pages/voting.js
@@ -1,1 +1,13 @@
-rfce
+import React from 'react'
+//Pages
+import {voting} from './Pages/voting|'
+ 
+import { Routes, Route } from "react-router-dom";
+function vote() {
+  return (
+    <Routes>
+       <Route path="/" element={  <voting/> }/>
+    </Routes>
+  )
+}
+export default vote
