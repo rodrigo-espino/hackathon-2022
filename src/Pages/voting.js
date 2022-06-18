e2d81c5186a6ef5b19d2e2f2d5a2fabd336d246a
Carlos Mora <eduardo.53.cm@gmail.com>
2022-06-18 10:53:11 -0500

    Kill us please

--- src/Pages/voting.js
+++ src/Pages/voting.js
@@ -3,16 +3,58 @@ import React from 'react'
 export function Vote() {
   return (
     <>
-    <button type="button" className="btn btn-secondary">Secondary</button>
-    <button type="button" className="btn btn-secondary">Secondary</button>
-    <button type="button" className="btn btn-secondary">Secondary</button>
-    <div className="card"></div>
-      <img src="" className="" alt=""/>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end"></div>
+      <div className="row">
+    <div className="col-md-6">
+      <button type="button" className="btn btn-secondary">Votos restantes</button>
+      </div>
+      <div className="col-md-6">
+       <button class="btn btn-primary me-md-2" type="button">Log Out</button>
+       </div>
+       <div className="col-md-6">
+<br />
+<br />
+       </div>
+       </div>
+       <div className="row">
+        <div className="col-md-3">
+          <center>
+            <img src="jonhy.jpg" className="Candidato" />
+          </center>
+          <center>
+            <h3>Jonhy Deep</h3>
+            <a href="#" className="btn btn-primary">Votar</a>
+          </center>
+        </div>
+        <div className="col-md-3">
+          <center>
+            <img src="KatyPerry.jpg" className="Candidato" />
+          </center>
+          <center>
+            <h3>Katy Perry</h3>
+            <a href="#" className="btn btn-primary">Votar</a>
+          </center>
+        </div>
+        <div className="col-md-3">
+          <center>
+            <img src="TomHolland.jpg" className="Candidato" />
+          </center>
+          <center>
+            <h3>Tom Holland</h3>
+            <a href="#" className="btn btn-primary">Votar</a>
+          </center>
+        </div>
+        <div className="col-md-3">
+          <center>
+            <img src="AndrewGarfield.jpg" className="Candidato" />
+          </center>
+          <center>
+            <h3>Andrew Garfield</h3>
+            <a href="#" className="btn btn-primary">Votar</a>
+          </center>
+        </div>
+        </div>
+
           </>
         )
       }
